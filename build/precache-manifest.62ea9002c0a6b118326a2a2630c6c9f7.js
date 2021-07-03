5638a3de5f57ea66191da71e459f6e8440e0c3b5
Niku_419 <2019198@iiitdmj.ac.in>
2021-07-03 23:35:55 +0530

    :dart: trying to deploy

---
+++ build/precache-manifest.62ea9002c0a6b118326a2a2630c6c9f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "209b29f1d3df997c41609b56612e59e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14fa4cd0db60a8d622af",
+    "url": "/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "0149fd2dccebe8e6564f",
+    "url": "/static/css/main.c39edb6f.chunk.css"
+  },
+  {
+    "revision": "14fa4cd0db60a8d622af",
+    "url": "/static/js/2.bd1a1c54.chunk.js"
+  },
+  {
+    "revision": "1f3ec544f59db43d224e4cd99b0c3b1d",
+    "url": "/static/js/2.bd1a1c54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0149fd2dccebe8e6564f",
+    "url": "/static/js/main.67c38a23.chunk.js"
+  },
+  {
+    "revision": "7d7e4c049bc5e15cdbdd",
+    "url": "/static/js/runtime-main.91e5ec65.js"
+  }
+]);
